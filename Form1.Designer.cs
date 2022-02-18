7bdfa45677bb3b489010d0608e4a805cb07656c6
Angedon <83006758+Angedon@users.noreply.github.com>
2022-02-18 21:07:48 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button_start = new System.Windows.Forms.Button();
+            this.button_choose = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            //this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(312, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(173, 33);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Ход игрока:";
+            // 
+            // button9
+            // 
+            this.button9.AccessibleName = "8";
+            this.button9.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button9.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button9.Location = new System.Drawing.Point(217, 224);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(72, 77);
+            this.button9.TabIndex = 8;
+            this.button9.UseVisualStyleBackColor = false;
+            // 
+            // button8
+            // 
+            this.button8.AccessibleName = "7";
+            this.button8.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button8.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button8.Location = new System.Drawing.Point(114, 224);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(72, 77);
+            this.button8.TabIndex = 7;
+            this.button8.UseVisualStyleBackColor = false;
+            // 
+            // button7
+            // 
+            this.button7.AccessibleName = "6";
+            this.button7.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button7.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button7.Location = new System.Drawing.Point(12, 224);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(72, 77);
+            this.button7.TabIndex = 6;
+            this.button7.UseVisualStyleBackColor = false;
+            // 
+            // button6
+            // 
+            this.button6.AccessibleName = "5";
+            this.button6.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button6.Location = new System.Drawing.Point(217, 127);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(72, 77);
+            this.button6.TabIndex = 5;
+            this.button6.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            this.button5.AccessibleName = "4";
+            this.button5.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button5.Location = new System.Drawing.Point(114, 127);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(72, 77);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.AccessibleName = "3";
+            this.button4.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button4.Location = new System.Drawing.Point(12, 127);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(72, 77);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.AccessibleName = "2";
+            this.button3.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button3.Location = new System.Drawing.Point(217, 24);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(72, 77);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.AccessibleName = "1";
+            this.button2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.Location = new System.Drawing.Point(114, 24);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(72, 77);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.AccessibleName = "0";
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(12, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(72, 77);
+            this.button1.TabIndex = 0;
+            this.button1.Tag = "1";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button_start
+            // 
+            this.button_start.Location = new System.Drawing.Point(344, 320);
+            this.button_start.Name = "button_start";
+            this.button_start.Size = new System.Drawing.Size(106, 47);
+            this.button_start.TabIndex = 11;
+            this.button_start.Text = "Начать";
+            this.button_start.UseVisualStyleBackColor = true;
+            // 
+            // button_choose
+            // 
+            this.button_choose.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button_choose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_choose.ForeColor = System.Drawing.Color.Transparent;
+            this.button_choose.Image = global::WindowsFormsApp1.Properties.Resources.Crest;
+            this.button_choose.Location = new System.Drawing.Point(370, 71);
+            this.button_choose.Name = "button_choose";
+            this.button_choose.Size = new System.Drawing.Size(49, 48);
+            this.button_choose.TabIndex = 12;
+            this.button_choose.Tag = "1";
+            this.button_choose.UseVisualStyleBackColor = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Enabled = false;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(12, 336);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 31);
+            this.label2.TabIndex = 13;
+            // 
+            // webBrowser1
+            // 
+            //this.webBrowser1.Location = new System.Drawing.Point(508, 42);
+            //this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            //this.webBrowser1.Name = "webBrowser1";
+            //this.webBrowser1.Size = new System.Drawing.Size(721, 585);
+            //this.webBrowser1.TabIndex = 14;
+            //this.webBrowser1.Url = new System.Uri("https://www.youtube.com/?gl=RU&hl=ru", System.UriKind.Absolute);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(700, 496);
+            //this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button_choose);
+            this.Controls.Add(this.button_start);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Заголовок";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button_start;
+        private System.Windows.Forms.Button button_choose;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+
+ 
+    public partial class Form1 : Form
+    {
+        
+        Crestic game = new Crestic();
+        public Form1()
+        {
+           
+
+            InitializeComponent();
+            button_start.Click += Button_start_Click;
+            button_choose.Click += Button_choose_Click;
+            button1.Click += Button1_Click;
+            button2.Click += Button1_Click;
+            button3.Click += Button1_Click;
+            button4.Click += Button1_Click;
+            button5.Click += Button1_Click;
+            button6.Click += Button1_Click;
+            button7.Click += Button1_Click;
+            button8.Click += Button1_Click;
+            button9.Click += Button1_Click;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (game.gamestart && !game.gameend)
+            {
+                int n = Convert.ToInt32(((Button)sender).AccessibleName);
+                int result = game.SelectPos(n, ref sender, ref button_choose);
+                if (result == 3) { label2.Enabled = true; label2.Text = "Неверный ход"; }
+                else if(result == 4) { label2.Enabled = true; label2.Text = "Ничья"; }
+                else if (result == 2) { label2.Enabled = true; label2.Text = "Крестики победили"; }
+                else if (result == 1) { label2.Enabled = true; label2.Text = "Нолики победили"; }
+                else { label2.Enabled = false; label2.Text = ""; }
+            }
+        }
+
+        private void Button_choose_Click(object sender, EventArgs e)
+        {
+            game.Change(ref button_choose);
+    
+        }
+
+        private void Button_start_Click(object sender, EventArgs e)
+        {
+            game.Start(ref button_choose, ref button_start, ref button1, ref button2, ref button3, ref button4, ref button5, ref button6, ref button7,
+                ref button8, ref button9);
+            label2.Text = "";
+        }
+
+    }
+    class Crestic
+    {
+       
+        private bool IsCrestic;
+        public bool gamestart;
+        public bool gameend;
+        private int[] area = new int[9];
+        public Crestic()
+        {
+            IsCrestic = true;
+            for (int i = 0; i < 9; i++)
+            {
+                area[i] = 0;
+            }
+        }
+       
+        public void Start(ref Button a, ref Button start, ref Button b1, ref Button b2, ref Button b3, ref Button b4, ref Button b5, ref Button b6,
+            ref Button b7, ref Button b8, ref Button b9)
+        {
+            if (gamestart)
+            {
+                End(ref a, ref start, ref b1, ref  b2, ref  b3, ref  b4, ref  b5, ref  b6,
+            ref  b7, ref  b8, ref b9);
+            }
+            else
+            {
+              
+                gameend = false;
+               gamestart = true;
+                start.Text = "Заново";
+            }
+        }
+        private void End(ref Button a, ref Button start, ref Button b1, ref Button b2, ref Button b3, ref Button b4, ref Button b5, ref Button b6,
+            ref Button b7, ref Button b8, ref Button b9)
+        {
+            gamestart = false;
+            IsCrestic = true;
+            a.Image = WindowsFormsApp1.Properties.Resources.Crest;
+            start.Text = "Начать";
+            for (int i = 0; i < 9; i++)
+            {
+                area[i] = 0;
+            }
+            b1.Image = null;
+            b2.Image = null;
+            b3.Image = null;
+            b4.Image = null;
+            b5.Image = null;
+            b6.Image = null;
+            b7.Image = null;
+            b8.Image = null;
+            b9.Image = null;
+        }
+
+        public void Change(ref Button a)
+        {
+            if (!gamestart)
+                IsCrestic = !IsCrestic;
+            if (IsCrestic)
+                a.Image = WindowsFormsApp1.Properties.Resources.Crest;
+            else a.Image = WindowsFormsApp1.Properties.Resources.Noll;
+        }
+        public int SelectPos(int x,ref object a, ref Button c)
+        {
+          if(!gameend)
+            if (area[x] != 0) return 3;
+            else
+            {
+                area[x] = Convert.ToInt32(IsCrestic) + 1;
+               if(area[x]==2)  ((Button)a).Image = WindowsFormsApp1.Properties.Resources.Crest;
+                if (area[x] == 1) ((Button)a).Image = WindowsFormsApp1.Properties.Resources.Noll;
+                    if (Win()) { gameend = true; return Convert.ToInt32(IsCrestic) + 1; }
+                    
+                IsCrestic = !IsCrestic;
+                    if (DoubleNichia()) { gameend = true; return 4; }
+                    if (IsCrestic) c.Image = WindowsFormsApp1.Properties.Resources.Crest;
+                    else c.Image = WindowsFormsApp1.Properties.Resources.Noll ;
+                }
+            return 0;
+        }
+        private bool Win()
+        {
+            if (area[0] == area[1] && area[1] == area[2] && area[0] != 0
+                || area[3] == area[4] && area[4] == area[5] && area[3] != 0
+                || area[6] == area[7] && area[7] == area[8] && area[6] != 0)
+            {
+                return true;
+            }
+            if (area[0] == area[3] && area[3] == area[6] && area[0] != 0
+                || area[1] == area[4] && area[4] == area[7] && area[1] != 0
+                || area[2] == area[5] && area[5] == area[8] && area[2] != 0)
+            {
+                return true;
+            }
+            if (area[0] == area[4] && area[4] == area[8] && area[0] != 0
+                || area[2] == area[4] && area[4] == area[6] && area[2] != 0)
+            {
+                return true;
+            }
+            return false;
+
+        }
+        private bool DoubleNichia()
+        {
+
+        
+            int c = 0;
+            for(int i=0;i<9;i++)
+            {
+                if (area[i] == 0)
+                {
+                    area[i] = Convert.ToInt32(IsCrestic) + 1;
+                    if (!Nichia()) c++;
+                    area[i] = 0;
+                }
+            }
+            if (c == 0) return true;
+            else return false;
+        }
+        private bool Nichia()
+        {
+            int[] a = new int[9];
+            area.CopyTo(a, 0);
+            int c = 0;
+            for(int i=0;i<9;i++)
+            {
+                if (area[i] == 0) area[i] = 1;
+            }
+            if (Win()) c++;
+            a.CopyTo(area, 0);
+            for(int i=0;i<9;i++)
+            {
+                if (area[i] == 0) area[i] = 2;
+            }
+            if (Win()) c++;
+            a.CopyTo(area, 0);
+            if (c == 0) return true;
+            else return false;
+        }
+
+    }
+
+}
